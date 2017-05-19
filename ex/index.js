31122363d3968322196a922fca414fae50a2cb62
Felipe Cavalcanti <felipecavalcanti100@gmail.com>
2017-05-19 10:54:50 -0300

    Conclusão da aula 14

--- ex/index.js
+++ ex/index.js
@@ -1,12 +1,6 @@
-const produto = {
-    nome: 'Caneta bic preta',
-    preco: 1.00
-}
+import 'react'
 
-function clone(objeto) {
-    return {...objeto}
-}
-
-const novoProduto = clone(produto);
-novoProduto.nome = 'Caneta bic azul';
-console.log(produto, novoProduto)
+export default props => (
+    <div>Teste</div>
+);
+console.log('Tudo OK');
