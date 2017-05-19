38825bee2b0e244e1f7fbf40413684403c93726a
Felipe Cavalcanti <felipecavalcanti100@gmail.com>
2017-05-19 10:37:56 -0300

    Conclusão da aula 13

--- ex/index.js
+++ ex/index.js
@@ -1,3 +1,12 @@
-import Pessoa from './pessoa'
-const pessoa = new Pessoa('Felipe')
-console.log(pessoa.toString())
+const produto = {
+    nome: 'Caneta bic preta',
+    preco: 1.00
+}
+
+function clone(objeto) {
+    return {...objeto}
+}
+
+const novoProduto = clone(produto);
+novoProduto.nome = 'Caneta bic azul';
+console.log(produto, novoProduto)
